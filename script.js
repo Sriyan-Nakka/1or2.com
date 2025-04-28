ddbf3a815e4ee966da88f9df9c9526d658979269
Sriyan-Nakka <sriyannakka@gmail.com>
2025-04-28 16:12:02 +0530

    version 0.31

---
+++ script.js
@@ -0,0 +1,15 @@
+document.querySelector("#singleplayerButton").onclick = function () {
+  document.querySelector("#singleplayerButton").style.display = "none";
+  document.querySelector("#multiplayerButton").style.display = "none";
+  document.querySelector("#gameContainer").style.display = "block";
+  document.querySelector("#images").style.display = "block";
+  //in progress...
+};
+
+document.querySelector("#multiplayerButton").onclick = function () {
+  //in progress...
+};
+
+document.querySelector("#restartButton").onclick = function () {
+  //in progress...
+};
