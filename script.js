b13d95deae0f5c1a7106913dd4f33230d040cebf
Sriyan-Nakka <sriyannakka@gmail.com>
2025-04-30 11:48:41 +0530

    version 0.45 + code optimizations

--- script.js
+++ script.js
@@ -2,15 +2,17 @@ let p1Name = "";
 let p2Name = "";
 let p1Points = 0;
 let p2Points = 0;
-let p1Turn = true;
-let p2Turn = false;
+let goalNum;
+let playMode = "";
 
 document.querySelector("#singleplayerButton").onclick = function () {
   playGame("singleplayer");
+  playMode = "singleplayer";
 };
 
 document.querySelector("#multiplayerButton").onclick = function () {
   playGame("multiplayer");
+  playMode = "multiplayer";
 };
 
 document.querySelector("#restartButton").onclick = function () {
@@ -32,31 +34,41 @@ document.querySelector("#restartButton").onclick = function () {
 };
 
 function playGame(mode) {
+  while (true) {
+    goalNum = Number(prompt("Enter a goal number between 2 and 10."));
+    if (
+      goalNum >= 2 &&
+      goalNum <= 10 &&
+      goalNum != null &&
+      goalNum != undefined
+    ) {
+      alert("The goal number is " + goalNum + ".");
+      break;
+    } else {
+      alert("Please enter a valid number between 2 and 10.");
+    }
+  }
+
+  p1Points = 0;
+  p2Points = 0;
+  document.querySelector("#p1PointsSpan").textContent = p1Points;
+  document.querySelector("#p2PointsSpan").textContent = p2Points;
+
+  document.querySelector("#singleplayerButton").style.display = "none";
+  document.querySelector("#multiplayerButton").style.display = "none";
+  document.querySelector("#gameContainer").style.display = "block";
+  document.querySelector("#images").style.display = "block";
+  document.querySelector("#restartButton").style.display = "inline-block";
+
   switch (mode) {
     case "singleplayer":
-      p1Points = 0;
-      p2Points = 0;
-      document.querySelector("#p1PointsSpan").textContent = p1Points;
-      document.querySelector("#p2PointsSpan").textContent = p2Points;
-
       p1Name = "You";
       p2Name = "Bot";
       document.querySelector("#p1Name").textContent = p1Name;
       document.querySelector("#p2Name").textContent = p2Name;
-
-      document.querySelector("#singleplayerButton").style.display = "none";
-      document.querySelector("#multiplayerButton").style.display = "none";
-      document.querySelector("#gameContainer").style.display = "block";
-      document.querySelector("#images").style.display = "block";
-      document.querySelector("#restartButton").style.display = "inline-block";
       break;
 
     case "multiplayer":
-      p1Points = 0;
-      p2Points = 0;
-      document.querySelector("#p1PointsSpan").textContent = p1Points;
-      document.querySelector("#p2PointsSpan").textContent = p2Points;
-
       //loop for Player 1 Name:
       while (true) {
         p1Name = prompt(
@@ -84,19 +96,27 @@ function playGame(mode) {
           alert("Please enter a valid name for Player 2.");
         }
       }
+      break;
+  }
+  document.querySelector("#playerNamePick").textContent = p1Name;
+}
 
-      document.querySelector("#singleplayerButton").style.display = "none";
-      document.querySelector("#multiplayerButton").style.display = "none";
-      document.querySelector("#gameContainer").style.display = "block";
-      document.querySelector("#images").style.display = "block";
-      document.querySelector("#restartButton").style.display = "inline-block";
+//playersTurn functions:
+document.querySelector("#num1").onclick = function () {
+  //in progress...
+  botTurn(playMode);
+};
+
+document.querySelector("#num2").onclick = function () {
+  //in progress...
+  botTurn(playMode);
+};
+
+function botTurn(mode) {
+  switch (mode) {
+    case "singleplayer":
+      break;
+    case "multiplayer":
       break;
   }
 }
-
-document.querySelector("#num1").onclick = function () {
-  //in progress...
-};
-document.querySelector("#num2").onclick = function () {
-  //in progress...
-};
